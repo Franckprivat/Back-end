2505b9e8ab4b3a42aa080a51dff9ffdb003b9404
Franck kiemde <kiemdefranck5@gmail.com>
2024-11-03 22:18:59 +0100

    Back

---
+++ backend/config/db.js
@@ -0,0 +1,16 @@
+// const mongoose = require('mongoose');
+
+// const connectDB = () => {
+//     try {
+//         mongoose.set('strictQuery', false);
+//         mongoose.connect(process.env.MONGO_URI, () =>
+//             console.log("MONGO connecté")
+//         );
+//     } catch (err) {
+//         console.log(err);
+//         process.exit();
+//     }
+
+// };
+
+// module.exports = connectDB
